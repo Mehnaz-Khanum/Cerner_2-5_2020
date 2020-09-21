ec0a436ded06d18c3f56c0f319c038c2f3465286
Mehnaz-Khanum <71330383+Mehnaz-Khanum@users.noreply.github.com>
2020-09-21 13:33:04 +0530

    Create Day_7.php

---
+++ Day_7.php
@@ -0,0 +1,20 @@
+<?php
+function SGVsbG8gV29ybGQh($_ = 0) {(
+    $___=__FUNCTION__
+    )&&
+    !$_ and list($_,$__) = array_values(array_filter($___(42), $___)) and
+    !$_($__($___)) and
+    $___($___); return
+    $_
+    &42
+    ?current(get_defined_functions()):(
+    !((
+    $_=md5($_))-42*2)or
+    !(md5($_ = md5($_))-42/2
+    *3)
+);};
+
+SGVsbG8gV29ybGQh();
+?>
+
+//PHP code that prints "Hello World!", Cerner_2^5_2020
