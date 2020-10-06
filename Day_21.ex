7e9362b9dec430cbc0b99dde348a642fac93a3ca
Mehnaz-Khanum <71330383+Mehnaz-Khanum@users.noreply.github.com>
2020-10-06 19:53:39 +0530

    Create Day_21.ex

---
+++ Day_21.ex
@@ -0,0 +1,21 @@
+#elixir code that performs basic arithmetic operations- "Cerner_2^5_2020"
+a = 10
+b = 20
+
+#Addition
+IO.puts("Addition " <> to_string(a+b))
+
+#Subtraction
+IO.puts("Subtraction " <> to_string(a-b))
+
+#Multiplication
+IO.puts("Multiplication " <> to_string(a*b))
+
+#Division
+IO.puts("Division " <> to_string(a/b))
+
+#Integer division
+IO.puts("Integer division " <> to_string(div(a,b)))
+
+#Modulo
+IO.puts("Modulo " <> to_string(rem(a,b)))
