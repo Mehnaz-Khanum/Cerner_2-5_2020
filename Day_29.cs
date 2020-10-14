76dcfe34ff8280db2dd0380956a0f97ef962d703
Mehnaz-Khanum <71330383+Mehnaz-Khanum@users.noreply.github.com>
2020-10-14 20:12:21 +0530

    Create Day_29.cs

---
+++ Day_29.cs
@@ -0,0 +1,2 @@
+//Obfuscated C# code that prints Hello World- "Cerner_2^5_2020"
+using System;struct a{static int Main(){object[]c={"\u0048e\x6c\x6co "+(C\u0068ar)(86+1)+"or\x6c\x64"};typeof(Conso\u006ce).GetMet\u0068o\u0064s()[101].Invoke(c,c);return 0;}}
